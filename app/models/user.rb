b4ea4121f04731e4ebfb784345f2b45f666d042a
mateomh <mmojicah.84@gmail.com>
2020-09-07 14:30:53 -0500

    Create the Post model and add validations to it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :name, :email, presence: true
-  validates :name, :email, uniqueness: true 
+  validates :name, :email, uniqueness: true
+  has_many :posts
 end
